e8f8326d17667a4574f7be99226fa41b1d0c6fd9
Brenda Wihogora <brewihogora23@gmail.com>
2022-10-24 22:37:51 +0200

    Add the association and validation for models

--- app/models/traffic.rb
+++ app/models/traffic.rb
@@ -1,2 +1,7 @@
 class Traffic < ApplicationRecord
+    belongs_to :user
+  belongs_to :category 
+
+    validates :name, presence: true
+      validates :amount, numericality: {greater_than_or_equal_to: 0 }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ApplicationRecord
+ has_many :category, dependent: :destroy
+ has_many :traffic, dependent: :destroy
+ validates :name, presence: true
 end
