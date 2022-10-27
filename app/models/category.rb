4bad80d0ca8532e3bf4155e4fd1d12a0481b7bf5
Brenda Wihogora <brewihogora23@gmail.com>
2022-10-27 18:19:37 +0200

    Fix linters

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,11 +1,10 @@
 class Category < ApplicationRecord
-    belongs_to :user
-    has_many:traffic , dependent: :destroy
-   validates :name, presence: true
-   validates :icon, presence: true
+  belongs_to :user
+  has_many :traffic, dependent: :destroy
+  validates :name, presence: true
+  validates :icon, presence: true
   validates :icon, allow_blank: true, format: {
-  with: %r{\.gif|jpg|png}i,
-  message: 'must be a url for gif, jpg, or png image.'
-}
+    with: /%r{\.gif|jpg|png}i/,
+    message: 'must be a url for gif, jpg, or png image.'
+  }
 end
-
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,14 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-    subject {User.new(name: 'Brenda', email: 'brenda@gmail.com', password: '123456')}
-    before {subject.save}
+  subject { User.new(name: 'Brenda', email: 'brenda@gmail.com', password: '123456') }
+  before { subject.save }
 
-    it 'the name should be present' do 
-        subject.name = nil
-        expect(subject).to_not be_valid
-    end
-it 'it should be  valid' do 
-        expect(subject).to be_valid
-    end
+  it 'the name should be present' do
+    subject.name = nil
+    expect(subject).to_not be_valid
+  end
+  it 'it should be  valid' do
+    expect(subject).to be_valid
+  end
 end
